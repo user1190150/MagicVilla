158a713528b79ff5e270b17826051beaf830dca5
user1190150 <besirevic205@gmail.com>
2025-01-31 21:49:38 +0100

    New Endpoint : GET implemented for getting a specific Villa obj from the villaList. It takes a parameter int id.
    #In the Endpoint itself we create a VillaDTO obj , then we search in the villaList with the Method FirstOrDefault the specific obj via id. when we find it we store it in villa and return it as a VillaDTO obj.
    !Id could be NULL.

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -6,17 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 namespace MagicVilla_VillaAPI.Controllers
 {
 	//Could be implemented like that [Route("api/[controller]")]
-	[Route("api/villaAPI")]
+	[Route("api/VillaAPI")]
 	[ApiController]
 	public class VillaAPIController : ControllerBase
 	{
 
-		//Endpoint.
+		//Endpoint. For getting the List of Villas.
 		//Swagger will identify this Endpoint as a GET Endpoint.
 		[HttpGet]
 		public IEnumerable<VillaDTO> GetVillas()
 		{
 			return VillaStore.villaList;
+		}
+
+		//Endpoint. For getting one specific Villa.
+		//or [HttpGet("{id:int}")] then id must be a integer.
+		[HttpGet("id")]
+		public VillaDTO GetVilla(int id)
+		{
+			VillaDTO villa = new VillaDTO();
+			villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+			return villa;
 		}
 	}
 }
