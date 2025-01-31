27be13abc2c8d4c3229ad44239bf3ef0b7403296
user1190150 <besirevic205@gmail.com>
2025-01-31 17:24:42 +0100

    Configure Swagger.

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -4,18 +4,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi(); 
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "VillaAPI");
-    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    //ver 1.0
+    //app.MapOpenApi();
+    //app.UseSwaggerUI(options =>
+    //{
+    //    options.SwaggerEndpoint("/openapi/v1.json", "VillaAPI");
+    //});
 }
 
 app.UseHttpsRedirection();
