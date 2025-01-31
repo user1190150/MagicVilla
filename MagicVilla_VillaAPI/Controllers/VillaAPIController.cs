6e7e4b958b3ad34dadccd579db74559d40c65e11
user1190150 <besirevic205@gmail.com>
2025-01-31 19:32:17 +0100

    Added Model Villa and Modified API VillaAPIController.
    #Added Class Villa with Id and Name Properties.
    #API Controller annotated with Route and ApiController Annotation. Route set on api/VillaApi.
    #Implemented first GET Endpoint wich returns a List of Villa's.
    #Endpoint was tested in Swagger with success. It returns a List of 2 obj of Villa with Id and Name. It works.

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+	[Route("api/VillaAPI")]
 	[ApiController]
 	public class VillaAPIController : ControllerBase
 	{
+
+		//Endpoint.
+		//Swagger will identify this Endpoint as a GET Endpoint.
+		[HttpGet]
+		public IEnumerable<Villa> GetVillas()
+		{
+			return new List<Villa> { 
+			new Villa{Id=1, Name="Pool View"},
+			new Villa{Id=2, Name="Beach View"}
+			};
+		}
 	}
 }
