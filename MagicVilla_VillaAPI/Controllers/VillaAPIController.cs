a012cf3afdd62ad3749e440f0a7056b7a7afd7bb
user1190150 <besirevic205@gmail.com>
2025-02-04 09:05:07 +0100

    Ersetzt `return Ok(villaDTO); durch return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
    #CreatedAtRoute gibt nun eine 201 Created Antwort zurück, um den RESTful-Standard einzuhalten.
    
    #GetVilla ist der Name der Route, die verwendet wird, um die neu erstellte Villa abzurufen.
    
    #Ein anonymer Typ { id = villaDTO.Id } wurde erstellt, um die ID der neuen Villa in die Route zu übergeben.
    
    #Das villaDTO-Objekt wird als Antwort gesendet, damit der Client die gespeicherten Daten sofort sehen kann.
    
    #Durch diese Änderung erhält der Client nicht nur eine Bestätigung der Erstellung, sondern auch die korrekte URL, unter der die neue Villa abrufbar ist

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -22,7 +22,7 @@ namespace MagicVilla_VillaAPI.Controllers
 
 		//Endpoint. For getting one specific Villa.
 		//or [HttpGet("{id:int}")] then id must be a integer.
-		[HttpGet("id")]
+		[HttpGet("{id:int}", Name = "GetVilla")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -45,7 +45,7 @@ namespace MagicVilla_VillaAPI.Controllers
 		}
 
 		[HttpPost]
-		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status201Created)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
@@ -64,7 +64,7 @@ namespace MagicVilla_VillaAPI.Controllers
 			villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
 			VillaStore.villaList.Add(villaDTO);
 
-			return Ok(villaDTO);
+			return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
 		}
 	}
 }
