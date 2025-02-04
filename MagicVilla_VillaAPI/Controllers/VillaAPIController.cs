c8497ca1d92a3d9852cb6c0afd60abe78bd49eee
user1190150 <besirevic205@gmail.com>
2025-02-04 10:23:15 +0100

    Benutzerdefinierte Validierung hinzugefügt, um sicherzustellen, dass keine zwei Villen denselben Namen haben.
    #FirstOrDefault überprüft, ob bereits eine Villa mit dem gleichen Namen existiert.
    
    #Falls der Name bereits existiert:
    Ein ModelState-Fehler wird mit der Nachricht          Villa already Exists! hinzugefügt.
    
    Die API gibt eine 400 Bad Request-Antwort zurück, um den Client zu informieren.
    
    ToLower() wird verwendet, um die Überprüfung  zu machen (Groß-/Kleinschreibung spielt keine Rolle).
    
    Dadurch wird verhindert, dass doppelte Villen im VillaStore.villaList gespeichert werden.

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -50,6 +50,11 @@ namespace MagicVilla_VillaAPI.Controllers
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
 		{
+			if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+			{
+				ModelState.AddModelError("CustomError", "Villa already Exists!");
+				return BadRequest(ModelState);
+			}
 
 			if (villaDTO == null)
 			{
