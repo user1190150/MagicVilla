af3cfe1388599e283201938d7df3b94c47911d1b
user1190150 <besirevic205@gmail.com>
2025-02-05 14:57:16 +0100

    Added AsNoTracking() in UpdatePartialVilla() to improve query performance
    Applied AsNoTracking() in UpdatePartialVilla(id, patchDTO) to prevent Entity Framework from tracking the retrieved villa.
    Ensures read efficiency since the entity is not modified directly.
    Reduces unnecessary memory usage and improves query performance.
    This change optimizes the partial update operation by treating the villa as read-only.
    Tracking is still handled correctly when the updated model is explicitly saved to the database.
    With this update, the PATCH endpoint is now more efficient, avoiding unnecessary entity tracking.

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -3,6 +3,7 @@ using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
@@ -159,7 +160,7 @@ namespace MagicVilla_VillaAPI.Controllers
 				return BadRequest();
 			}
 
-			var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+			var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
 
 			VillaDTO villaDTO = new()
 			{
