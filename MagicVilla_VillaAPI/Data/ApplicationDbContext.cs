0e957fb1efd388edc7c193339438e1742781abad
user1190150 <besirevic205@gmail.com>
2025-02-05 12:29:59 +0100

    Configured ApplicationDbContext with SQL Server and Dependency Injection" Registered ApplicationDbContext in the DI (Dependency Injection) container inside Program.cs using builder.Services.AddDbContext<ApplicationDbContext>().
    This ensures that ApplicationDbContext can be injected into controllers and services wherever database access is needed.
    Configured SQL Server as the database provider with UseSqlServer(), specifying the connection string from appsettings.json.
    
    The connection string "DefaultSQLConnection" defines the database server, database name, and authentication method.
    This allows EF Core to know which database to connect to and how.
    Added a constructor in ApplicationDbContext that takes DbContextOptions<ApplicationDbContext> as a parameter.
    
    This constructor passes the configuration options to the base class DbContext, enabling EF Core to apply the provided settings.
    The options include the database provider, connection string, and other configurations set in Program.cs.
    Purpose of this setup:
    
    Allows EF Core to establish a database connection and manage entities (DbSet<Villa>).
    Enables dependency injection, so ApplicationDbContext can be used in controllers and services without manually creating an instance.
    Provides a structured way to configure and extend database behavior, ensuring flexibility in the application.

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace MagicVilla_VillaAPI.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
 		public DbSet<Villa> Villas { get; set; }
 	}
 }
